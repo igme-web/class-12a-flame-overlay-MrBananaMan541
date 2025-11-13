78dace48bce3962aabf40b1c15b2e39b46d60c33
MrBananaMan541 <112341486+MrBananaMan541@users.noreply.github.com>
2025-11-13 02:25:41 -0500

    Finished In Class Exercise 12

--- week12a_flame_overlay_starter/lib/main.dart
+++ week12a_flame_overlay_starter/lib/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 
 import 'game.dart';
 
+import 'overlay_title.dart';
+import 'overlay_main.dart';
+import 'overlay_pause.dart';
+import 'overlay_info.dart';
+import 'overlay_settings.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
@@ -20,7 +26,28 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GameWidget(
-          game: OverlayTutorial(context),
+          game: OverlayTutorial(context)
+            ..paused = true, // Cascade to pause immediately
+          overlayBuilderMap: {
+            'title': (context, game) {
+              return OverlayTitle(game: game as OverlayTutorial);
+            },
+            'main': (context, game) {
+              return mainOverlay(context, game);
+            },
+            'pause': (context, game) {
+              return pauseOverlay(context, game);
+            },
+            'info': (context, game) {
+              return InfoOverlay(
+                game: game as OverlayTutorial,
+              ); // Cast required!
+            },
+            'settings': (context, game) {
+              return settingsOverlay(context, game);
+            },
+          },
+          initialActiveOverlays: const ['title'],
         ),
       ),
     );
